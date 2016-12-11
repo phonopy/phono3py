11e6f58a17498472fb04e669d9f6fc11bc1216cc
Atsushi Togo <atz.togo@gmail.com>
2016-12-11 09:33:28 +0900

    Update for conda build

--- conda/anaconda_deploy.sh
+++ conda/anaconda_deploy.sh
@@ -4,9 +4,6 @@ export LBL=$1
 export TKN=$2
 export GIT_BRANCH=$3
 
-echo "extra_link_args_lapacke = ['-lopenblas']" |tee libopenblas.py
-export C_INCLUDE_PATH=${HOME}/miniconda/include:${C_INCLUDE_PATH}
-export LD_LIBRARY_PATH=${HOME}/miniconda/lib:${LD_LIBRARY_PATH}
 conda install conda-build anaconda-client --yes
 conda config --add channels atztogo
 conda build conda --no-anaconda-upload
