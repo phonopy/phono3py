0a429fe765dafab26f1fc62f88b835056d78095e
Atsushi Togo <atz.togo@gmail.com>
2017-07-12 16:56:50 +0900

    conda build debugging

--- conda/build.sh
+++ conda/build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-sed s/version_from_shell/`git describe --tags --dirty | sed -e 's/-\(.*\)-g.*/+\1/' -e 's/^[vr]//g'`/ meta.yaml > meta.tmp.yaml
-mv meta.tmp.yaml meta.yaml
-
-./get_nanoversion.sh
-
 export C_INCLUDE_PATH=${HOME}/miniconda/include:${C_INCLUDE_PATH}
 export LD_LIBRARY_PATH=${HOME}/miniconda/lib:${LD_LIBRARY_PATH}
 echo "extra_link_args_lapacke = ['-lopenblas']" |tee libopenblas.py
