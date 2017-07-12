bee3f221da24891c0fef031793e911070ace0be4
Atsushi Togo <atz.togo@gmail.com>
2017-07-12 22:52:20 +0900

    Update conda setting for travis CI

--- conda/build.sh
+++ conda/build.sh
@@ -3,6 +3,8 @@
 export C_INCLUDE_PATH=${HOME}/.miniconda/include:${C_INCLUDE_PATH}
 export LD_LIBRARY_PATH=${HOME}/.miniconda/lib:${LD_LIBRARY_PATH}
 
+./get_nanoversion.sh
+
 $PYTHON setup.py install
 
 # Add more build steps here, if they are necessary.
