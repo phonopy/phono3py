2d1198015fd6c230678271e5b9676828420105d2
Atsushi Togo <atz.togo@gmail.com>
2017-07-12 16:49:37 +0900

    conda build debugging

--- conda/build.sh
+++ conda/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# git describe --tags --dirty | sed -e 's/-\(.*\)-g.*/+\1/' -e 's/^[vr]//g' > __conda_version__.txt
-
-# echo "1.11.3" > __conda_version__.txt
+sed s/version_from_shell/`git describe --tags --dirty | sed -e 's/-\(.*\)-g.*/+\1/' -e 's/^[vr]//g'`/ meta.yaml > meta.tmp.yaml
+mv meta.tmp.yaml meta.yaml
 
 ./get_nanoversion.sh
 
