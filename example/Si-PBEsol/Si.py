f0a722d1bba48d5362810fcd6d8241a5ef474917
Atsushi Togo <atz.togo@gmail.com>
2018-05-28 16:33:04 +0900

    Update phono3py script

--- example/Si-PBEsol/Si.py
+++ example/Si-PBEsol/Si.py
@@ -18,12 +18,9 @@ def run_thermal_conductivity(phono3py):
     # Create fc3 and fc2 from disp_fc3.yaml and FORCES_FC3
     disp_dataset = parse_disp_fc3_yaml(filename="disp_fc3.yaml")
     forces_fc3 = parse_FORCES_FC3(disp_dataset, filename="FORCES_FC3")
-    phono3py.produce_fc3(
-        forces_fc3,
-        displacement_dataset=disp_dataset,
-        is_translational_symmetry=True,
-        is_permutation_symmetry=True,
-        is_permutation_symmetry_fc2=True)
+    phono3py.produce_fc3(forces_fc3,
+                         displacement_dataset=disp_dataset,
+                         symmetrize_fc3r=True)
     fc3 = phono3py.get_fc3()
     fc2 = phono3py.get_fc2()
 
