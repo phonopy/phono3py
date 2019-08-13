6e6dd39473b97e1d965995fb5d823ec6b5160119
Atsushi Togo <atz.togo@gmail.com>
2019-08-13 13:04:00 +0900

    Moved API classes from phono3py/phonon3/ to phono3py/

--- phono3py/api_phono3py.py
+++ phono3py/api_phono3py.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2016 Atsushi Togo
 # All rights reserved.
 #
-# This file is part of phonopy.
+# This file is part of phono3py.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -57,11 +57,8 @@ from phono3py.phonon3.frequency_shift import get_frequency_shift
 from phono3py.phonon3.interaction import Interaction
 from phono3py.phonon3.conductivity_RTA import get_thermal_conductivity_RTA
 from phono3py.phonon3.conductivity_LBTE import get_thermal_conductivity_LBTE
-from phono3py.phonon3.joint_dos import JointDos
 from phono3py.phonon3.displacement_fc3 import (get_third_order_displacements,
                                                direction_to_displacement)
-from phono3py.file_IO import write_joint_dos
-from phono3py.other.isotope import Isotope
 from phono3py.phonon3.fc3 import (
     set_permutation_symmetry_fc3,
     set_permutation_symmetry_compact_fc3,
@@ -1220,183 +1217,3 @@ class Phono3py(object):
             msg = "mesh has inappropriate type."
             raise TypeError(msg)
         self._mesh_numbers = mesh_nums
-
-
-class Phono3pyIsotope(object):
-    def __init__(self,
-                 mesh,
-                 primitive,
-                 mass_variances=None,  # length of list is num_atom.
-                 band_indices=None,
-                 sigmas=None,
-                 frequency_factor_to_THz=VaspToTHz,
-                 symprec=1e-5,
-                 cutoff_frequency=None,
-                 lapack_zheev_uplo='L'):
-        if sigmas is None:
-            self._sigmas = [None]
-        else:
-            self._sigmas = sigmas
-        self._mesh_numbers = mesh
-        self._iso = Isotope(mesh,
-                            primitive,
-                            mass_variances=mass_variances,
-                            band_indices=band_indices,
-                            frequency_factor_to_THz=frequency_factor_to_THz,
-                            symprec=symprec,
-                            cutoff_frequency=cutoff_frequency,
-                            lapack_zheev_uplo=lapack_zheev_uplo)
-
-    def run(self, grid_points):
-        for gp in grid_points:
-            self._iso.set_grid_point(gp)
-
-            print("--------------- Isotope scattering ---------------")
-            print("Grid point: %d" % gp)
-            adrs = self._iso.get_grid_address()[gp]
-            q = adrs.astype('double') / self._mesh_numbers
-            print("q-point: %s" % q)
-
-            if self._sigmas:
-                for sigma in self._sigmas:
-                    if sigma is None:
-                        print("Tetrahedron method")
-                    else:
-                        print("Sigma: %s" % sigma)
-                    self._iso.set_sigma(sigma)
-                    self._iso.run()
-
-                    frequencies = self._iso.get_phonons()[0]
-                    print('')
-                    print("Phonon-isotope scattering rate in THz (1/4pi-tau)")
-                    print(" Frequency     Rate")
-                    for g, f in zip(self._iso.get_gamma(), frequencies[gp]):
-                        print("%8.3f     %5.3e" % (f, g))
-            else:
-                print("sigma or tetrahedron method has to be set.")
-
-    def set_dynamical_matrix(self,
-                             fc2,
-                             supercell,
-                             primitive,
-                             nac_params=None,
-                             frequency_scale_factor=None,
-                             decimals=None):
-        self._primitive = primitive
-        self._iso.set_dynamical_matrix(
-            fc2,
-            supercell,
-            primitive,
-            nac_params=nac_params,
-            frequency_scale_factor=frequency_scale_factor,
-            decimals=decimals)
-
-    def set_sigma(self, sigma):
-        self._iso.set_sigma(sigma)
-
-
-class Phono3pyJointDos(object):
-    def __init__(self,
-                 supercell,
-                 primitive,
-                 mesh,
-                 fc2,
-                 nac_params=None,
-                 nac_q_direction=None,
-                 sigmas=None,
-                 cutoff_frequency=1e-4,
-                 frequency_step=None,
-                 num_frequency_points=None,
-                 temperatures=None,
-                 frequency_factor_to_THz=VaspToTHz,
-                 frequency_scale_factor=None,
-                 is_mesh_symmetry=True,
-                 symprec=1e-5,
-                 output_filename=None,
-                 log_level=0):
-        if sigmas is None:
-            self._sigmas = [None]
-        else:
-            self._sigmas = sigmas
-        self._supercell = supercell
-        self._primitive = primitive
-        self._mesh_numbers = mesh
-        self._fc2 = fc2
-        self._nac_params = nac_params
-        self._nac_q_direction = nac_q_direction
-        self._cutoff_frequency = cutoff_frequency
-        self._frequency_step = frequency_step
-        self._num_frequency_points = num_frequency_points
-        self._temperatures = temperatures
-        self._frequency_factor_to_THz = frequency_factor_to_THz
-        self._frequency_scale_factor = frequency_scale_factor
-        self._is_mesh_symmetry = is_mesh_symmetry
-        self._symprec = symprec
-        self._filename = output_filename
-        self._log_level = log_level
-
-        self._jdos = JointDos(
-            self._mesh_numbers,
-            self._primitive,
-            self._supercell,
-            self._fc2,
-            nac_params=self._nac_params,
-            nac_q_direction=self._nac_q_direction,
-            cutoff_frequency=self._cutoff_frequency,
-            frequency_step=self._frequency_step,
-            num_frequency_points=self._num_frequency_points,
-            temperatures=self._temperatures,
-            frequency_factor_to_THz=self._frequency_factor_to_THz,
-            frequency_scale_factor=self._frequency_scale_factor,
-            is_mesh_symmetry=self._is_mesh_symmetry,
-            symprec=self._symprec,
-            filename=output_filename,
-            log_level=self._log_level)
-
-    def run(self, grid_points):
-        if self._log_level:
-            print("--------------------------------- Joint DOS "
-                  "---------------------------------")
-            print("Sampling mesh: [ %d %d %d ]" % tuple(self._mesh_numbers))
-
-        for i, gp in enumerate(grid_points):
-            self._jdos.set_grid_point(gp)
-
-            if self._log_level:
-                weights = self._jdos.get_triplets_at_q()[1]
-                print("======================= "
-                      "Grid point %d (%d/%d) "
-                      "=======================" % (gp, i + 1, len(grid_points)))
-                adrs = self._jdos.get_grid_address()[gp]
-                q = adrs.astype('double') / self._mesh_numbers
-                print("q-point: (%5.2f %5.2f %5.2f)" % tuple(q))
-                print("Number of triplets: %d" % len(weights))
-                print("Frequency")
-                for f in self._jdos.get_phonons()[0][gp]:
-                    print("%8.3f" % f)
-
-            if self._sigmas:
-                for sigma in self._sigmas:
-                    if self._log_level:
-                        if sigma is None:
-                            print("Tetrahedron method is used.")
-                        else:
-                            print("Smearing method with sigma=%s is used." % sigma)
-                    self._jdos.set_sigma(sigma)
-                    self._jdos.run()
-                    filename = self._write(gp, sigma=sigma)
-                    if self._log_level:
-                        print("JDOS is written into \"%s\"." % filename)
-            else:
-                if self._log_level:
-                    print("sigma or tetrahedron method has to be set.")
-
-    def _write(self, gp, sigma=None):
-        return write_joint_dos(gp,
-                               self._mesh_numbers,
-                               self._jdos.get_frequency_points(),
-                               self._jdos.get_joint_dos(),
-                               sigma=sigma,
-                               temperatures=self._temperatures,
-                               filename=self._filename,
-                               is_mesh_symmetry=self._is_mesh_symmetry)
