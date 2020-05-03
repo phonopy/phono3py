fbae8715e6aefa1adcc09ef332030da6077d5638
Atsushi Togo <atz.togo@gmail.com>
2020-05-03 11:54:06 +0900

    Update phonopy version dependency

--- setup.py
+++ setup.py
@@ -320,7 +320,7 @@ if __name__ == '__main__':
               url='http://phonopy.github.io/phono3py/',
               packages=packages_phono3py,
               install_requires=['numpy', 'scipy', 'PyYAML', 'matplotlib',
-                                'h5py', 'phonopy>=2.6,<2.7'],
+                                'h5py', 'phonopy>=2.7,<2.8'],
               provides=['phono3py'],
               scripts=scripts_phono3py,
               ext_modules=[extension_lapackepy, extension_phono3py],
