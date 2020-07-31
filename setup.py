9284abd1d9c4dc48ca46e2df6c8be4e06eb06be0
Atsushi Togo <atz.togo@gmail.com>
2020-07-31 11:01:46 +0900

    Set version v1.21.0 for development

--- setup.py
+++ setup.py
@@ -321,7 +321,7 @@ if __name__ == '__main__':
               url='http://phonopy.github.io/phono3py/',
               packages=packages_phono3py,
               install_requires=['numpy', 'scipy', 'PyYAML', 'matplotlib',
-                                'h5py', 'spglib', 'phonopy>=2.7,<2.8'],
+                                'h5py', 'spglib', 'phonopy>=2.8,<2.9'],
               provides=['phono3py'],
               scripts=scripts_phono3py,
               ext_modules=[extension_lapackepy, extension_phono3py])
