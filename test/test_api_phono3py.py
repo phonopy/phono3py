5cc2afb6f73917dbe70d79bf0250f03302bb9aaa
Atsushi Togo <atz.togo@gmail.com>
2020-08-23 10:59:47 +0900

    Loosen testing value tolerance for LBTE

--- test/test_api_phono3py.py
+++ test/test_api_phono3py.py
@@ -9,7 +9,7 @@ def test_kappa_RTA(si_pbesol):
     si_pbesol.init_phph_interaction()
     si_pbesol.run_thermal_conductivity(temperatures=[300, ])
     kappa = si_pbesol.thermal_conductivity.kappa.ravel()
-    np.testing.assert_allclose(si_pbesol_kappa_RTA, kappa, atol=0.1)
+    np.testing.assert_allclose(si_pbesol_kappa_RTA, kappa, atol=0.5)
 
 
 def test_kappa_LBTE(si_pbesol):
@@ -17,4 +17,4 @@ def test_kappa_LBTE(si_pbesol):
     si_pbesol.init_phph_interaction()
     si_pbesol.run_thermal_conductivity(is_LBTE=True, temperatures=[300, ])
     kappa = si_pbesol.thermal_conductivity.kappa.ravel()
-    np.testing.assert_allclose(si_pbesol_kappa_LBTE, kappa, atol=0.1)
+    np.testing.assert_allclose(si_pbesol_kappa_LBTE, kappa, atol=0.5)
