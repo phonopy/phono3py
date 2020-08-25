0dc352e83f705258e1dc21651e43bd892e56d611
Atsushi Togo <atz.togo@gmail.com>
2020-08-25 16:15:57 +0900

    LambdaTensor was removed.

--- phono3py/sscha/sscha.py
+++ phono3py/sscha/sscha.py
@@ -44,130 +44,7 @@ Formulae implemented are based on these papers:
 import numpy as np
 from phonopy.units import VaspToTHz
 from phonopy.harmonic.dynmat_to_fc import DynmatToForceConstants
-from phono3py.phonon.func import mode_length, bose_einstein
-
-
-class LambdaTensor(object):
-    def __init__(self, frequencies, eigenvectors, masses):
-        """
-
-        Parameters
-        ----------
-        frequencies : ndarray
-            Supercell phonon frequencies in THz (without 2pi).
-            shape=(grid_point, band), dtype='double', order='C'
-        eigenvectors : ndarray
-            Supercell phonon eigenvectors.
-            shape=(grid_point, band, band), dtype='double', order='C'
-        massses : ndarray
-            Atomic masses of supercell in AMU.
-
-        """
-
-        self._frequencies = frequencies
-        self._eigenvectors = eigenvectors
-        self._masses = masses
-        self._occ = None
-
-    def run(self, f, T):
-        r"""Calculate Lambda tensor with four legs of atomic indices
-
-        Sum over phonon modes mu and nu
-
-        .. math::
-
-           - \frac{\hbar^2}{8} \sum_{\mu\nu}
-           \frac{F(z, \omega_\mu, \omega_\nu)}{\omega_\mu \omega_\nu}
-           \frac{e^a_\mu}{\sqrt{M_a}} \frac{e^b_\nu}{\sqrt{M_b}}
-           \frac{e^c_\mu}{\sqrt{M_c}} \frac{e^d_\nu}{\sqrt{M_d}}
-
-        Parameters
-        ----------
-        f : float
-            Phonon freuqency in THz (without 2pi)
-        T : float
-            Temperature in K
-
-        """
-
-        freqs = self._frequencies
-        self._occ = bose_einstein(freqs, T)
-        sqrt_masses = np.sqrt(self._masses)
-        dtype = "c%d" % (np.dtype('double').itemsize * 2)
-        N = len(self._masses)
-        lambda_tensor = np.zeros((N, N, N, N, 9, 9), dtype=dtype, order='C')
-        fz = np.zeros((len(freqs), len(freqs)), dtype='double', order='C')
-
-        for a, m_a in enumerate(sqrt_masses):
-            for b, m_b in enumerate(sqrt_masses):
-                for c, m_c in enumerate(sqrt_masses):
-                    for d, m_d in enumerate(sqrt_masses):
-                        ph_sum = self._sum_over_phonons(f, freqs, a, b, c, d)
-                        ph_sum /= m_a * m_b * m_c * m_d
-                        lambda_tensor[a, b, c, d] = ph_sum
-
-    def _sum_over_phonons(self, f, freqs, a, b, c, d):
-        lambda_abcd = np.zeros((9, 9), dtype='double', order='C')
-        eigvecs = self._eigenvectors
-        for i, f_i in enumerate(freqs):
-            e_i = eigvecs[:, i].reshape(-1, 3)
-            for j, f_j in enumerate(freqs):
-                e_j = eigvecs[:, j].reshape(-1, 3)
-                fz = self._get_Fz(f, f_i, f_j, self._occ[i], self._occ[j])
-                fz /= f_i * f_j
-                tensor = np.outer(np.outer(e_i[a], e_j[b]).ravel(),
-                                  np.outer(e_i[c], e_j[d]).ravel())
-                lambda_abcd = fz * tensor
-        return lambda_abcd
-
-    def _get_Fz(self, f, f_1, f_2, n_1, n_2):
-        r"""Function in Lambda
-
-        .. math::
-
-           \frac{2}{\hbar} \left\{ \frac{(\omega_\mu + \omega_\nu)
-           (1 + n_\mu + n_\nu)]}{(\omega_\mu + \omega_\nu)^2 - z^2}
-           - \frac{(\omega_\mu - \omega_\nu)(n_\mu - n_\nu)]}
-           {(\omega_\mu - \omega_\nu)^2 - z^2} \right \}
-
-        Parameters
-        ----------
-        f : float
-            Phonon freuqency in THz (without 2pi)
-        T : float
-            Temperature in K
-
-        Returns
-        -------
-        Calculated value without hbar / 2.
-
-        """
-
-        f_sum = f_1 + f_2
-        f_diff = f_1 - f_2
-        fz = f_sum * (1 + n_1 + n_2) / (f_sum ** 2 - f ** 2)
-        fz -= f_diff * (n_1 - n_2) / (f_diff ** 2 - f ** 2)
-        return fz
-
-
-class DispCorrMatrix(object):
-    """Calculate gamma matrix"""
-
-    def __init__(self, supercell_phonon):
-        self._supercell_phonon = supercell_phonon
-        self._gamma_matrix = None
-
-    def run(self, T):
-        freqs = self._supercell_phonon.frequencies
-        eigvecs = self._supercell_phonon.eigenvectors
-        a = mode_length(freqs, T)
-        masses = np.repeat(self._supercell_phonon.supercell.masses, 3)
-        gamma = np.dot(eigvecs, np.dot(np.diag(1.0 / a ** 2), eigvecs.T))
-        self._gamma_matrix = gamma * np.sqrt(np.outer(masses, masses))
-
-    @property
-    def gamma_matrix(self):
-        return self._gamma_matrix
+from phono3py.phonon.func import mode_length
 
 
 class SupercellPhonon(object):
@@ -204,6 +81,26 @@ class SupercellPhonon(object):
     @property
     def supercell(self):
         return self._supercell
+
+
+class DispCorrMatrix(object):
+    """Calculate gamma matrix"""
+
+    def __init__(self, supercell_phonon):
+        self._supercell_phonon = supercell_phonon
+        self._gamma_matrix = None
+
+    def run(self, T):
+        freqs = self._supercell_phonon.frequencies
+        eigvecs = self._supercell_phonon.eigenvectors
+        a = mode_length(freqs, T)
+        masses = np.repeat(self._supercell_phonon.supercell.masses, 3)
+        gamma = np.dot(eigvecs, np.dot(np.diag(1.0 / a ** 2), eigvecs.T))
+        self._gamma_matrix = gamma * np.sqrt(np.outer(masses, masses))
+
+    @property
+    def gamma_matrix(self):
+        return self._gamma_matrix
 
 
 class DispCorrMatrixMesh(object):
--- test/sscha/test_sscha.py
+++ test/sscha/test_sscha.py
@@ -26,12 +26,6 @@ def get_supercell_phonon(ph3):
     return SupercellPhonon(supercell, fc2, factor=factor)
 
 
-def test_run_Lambda(si_pbesol_111):
-    sph = get_supercell_phonon(si_pbesol_111)
-    lt = LambdaTensor(sph.frequencies, sph.eigenvectors, sph.supercell.masses)
-    lt.run(10, 300)
-
-
 def test_SupercellPhonon(si_pbesol_111):
     sph = get_supercell_phonon(si_pbesol_111)
     np.testing.assert_allclose(
