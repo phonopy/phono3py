f9ffcc90fac3c242601898b9e9958b8615e898f1
Atsushi Togo <atz.togo@gmail.com>
2020-09-06 12:03:57 +0900

    Spectral function implementation updated

--- phono3py/phonon3/spectral_function.py
+++ phono3py/phonon3/spectral_function.py
@@ -53,26 +53,19 @@ class SpectralFunction(object):
         self._frequency_step = frequency_step
         self._num_frequency_points = num_frequency_points
         self._temperatures = temperatures
+        self._spectral_functions = None
+        self._gammas = None
+        self._deltas = None
+        self._frequency_points = None
 
     def run(self):
-        # gammas[grid_points, sigmas, temps, freq_points, band_indices]
-        gammas, self._frequency_points = get_imag_self_energy(
-            self._interaction,
-            self._grid_points,
-            frequency_points=self._frequency_points_in,
-            frequency_step=self._frequency_step,
-            num_frequency_points=self._num_frequency_points,
-            temperatures=self._temperatures)
-        self._gammas = np.array(gammas[:, 0], dtype='double', order='C')
-        self._deltas = np.zeros_like(self._gammas)
-        for i, gp in enumerate(self._grid_points):
-            for j, temp in enumerate(self._temperatures):
-                for k, bi in enumerate(self._interaction.band_indices):
-                    re_part, fpoints = imag_to_real(
-                        self._gammas[i, j, :, k], self._frequency_points)
-                    self._deltas[i, j, :, k] = -re_part
+        self._run_gamma()
+        self._run_delta()
+        self._run_spectral_function()
 
-        assert (np.abs(self._frequency_points - fpoints) < 1e-8).all()
+    @property
+    def spectral_functions(self):
+        return self._spectral_functions
 
     @property
     def shifts(self):
@@ -85,3 +78,43 @@ class SpectralFunction(object):
     @property
     def frequency_points(self):
         return self._frequency_points
+
+    def _run_gamma(self):
+        # gammas[grid_points, sigmas, temps, freq_points, band_indices]
+        gammas, self._frequency_points = get_imag_self_energy(
+            self._interaction,
+            self._grid_points,
+            frequency_points=self._frequency_points_in,
+            frequency_step=self._frequency_step,
+            num_frequency_points=self._num_frequency_points,
+            temperatures=self._temperatures)
+        self._gammas = np.array(gammas[:, 0], dtype='double', order='C')
+
+    def _run_delta(self):
+        self._deltas = np.zeros_like(self._gammas)
+        for i, gp in enumerate(self._grid_points):
+            for j, temp in enumerate(self._temperatures):
+                for k, bi in enumerate(self._interaction.band_indices):
+                    re_part, fpoints = imag_to_real(
+                        self._gammas[i, j, :, k], self._frequency_points)
+                    self._deltas[i, j, :, k] = -re_part
+        assert (np.abs(self._frequency_points - fpoints) < 1e-8).all()
+
+    def _run_spectral_function(self):
+        frequencies = self._interaction.get_phonons()[0]
+        self._spectral_functions = np.zeros_like(self._gammas)
+        for i, gp in enumerate(self._grid_points):
+            for j, temp in enumerate(self._temperatures):
+                for k, bi in enumerate(self._interaction.band_indices):
+                    freq = frequencies[gp, bi]
+                    gammas = self._gammas[i, j, :, k]
+                    deltas = self._deltas[i, j, :, k]
+                    vals = self._get_spectral_function(gammas, deltas, freq)
+                    self._spectral_functions[i, j, :, k] = vals
+
+    def _get_spectral_function(self, gammas, deltas, freq):
+        fpoints = self._frequency_points
+        vals = (4 * freq ** 2 * gammas) / (
+            (fpoints - freq ** 2 - 2 * freq * deltas) ** 2
+            + (2 * freq * gammas) ** 2)
+        return vals
