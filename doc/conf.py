4e30cf8e7c9377cf4b6c1c3a0c05c44194342f0e
Atsushi Togo <atz.togo@gmail.com>
2021-07-22 21:17:12 +0900

    rst files were transformed to md files.

--- doc/conf.py
+++ doc/conf.py
@@ -29,6 +29,7 @@ import os
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 # extensions = ['sphinx.ext.imgmath']
 extensions = ['sphinx.ext.mathjax', 'myst_parser']
+myst_enable_extensions = ["linkify", "dollarmath", "amsmath"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
