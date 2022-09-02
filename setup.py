0b1dbf8046510f4d60d12e26a2e11e38056aa5a7
Atsushi Togo <atz.togo@gmail.com>
2022-09-02 17:11:23 +0900

    Find static link libraries in setup.py

--- setup.py
+++ setup.py
@@ -94,22 +94,38 @@ def _get_extensions(build_dir):
     define_macros.append(("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION"))
     include_dirs += ["c", numpy.get_include()]
 
+    extra_link_args_ph3py = []
+    libph3py = list((pathlib.Path.cwd() / "_build").glob("*ph3py.*"))
+    if libph3py:
+        print("=============================================")
+        print(f"Phono3py library: {libph3py[0]}")
+        print("=============================================")
+        extra_link_args_ph3py += [str(libph3py[0])]
+
     extensions.append(
         setuptools.Extension(
             "phono3py._phono3py",
             sources=["c/_phono3py.c"],
-            extra_link_args=["_build/libph3py.a"] + extra_link_args,
+            extra_link_args=extra_link_args + extra_link_args_ph3py,
             include_dirs=include_dirs,
             extra_compile_args=extra_compile_args,
             define_macros=define_macros,
         )
     )
 
+    extra_link_args_phmod = []
+    libphmod = list((pathlib.Path.cwd() / "_build").glob("*phmod.*"))
+    if libphmod:
+        print("=============================================")
+        print(f"Phonon library: {libphmod[0]}")
+        print("=============================================")
+        extra_link_args_phmod += [str(libphmod[0])]
+
     extensions.append(
         setuptools.Extension(
             "phono3py._phononmod",
             sources=["c/_phononmod.c"],
-            extra_link_args=["_build/libphmod.a"] + extra_link_args,
+            extra_link_args=extra_link_args + extra_link_args_phmod,
             include_dirs=include_dirs,
             extra_compile_args=extra_compile_args,
             define_macros=define_macros,
