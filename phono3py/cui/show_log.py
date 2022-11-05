919fb544811b303a4d8ffb7d4ee5b56122730ad4
Atsushi Togo <atz.togo@gmail.com>
2022-11-05 23:30:43 +0900

    Allow specifying 9 values mesh from CLI

--- phono3py/cui/show_log.py
+++ phono3py/cui/show_log.py
@@ -220,12 +220,12 @@ def show_phono3py_settings(phono3py, settings, updated_settings, log_level):
 
     if settings.mesh_numbers is not None:
         try:
-            if len(settings.mesh_numbers) == 3:
-                mesh_numbers = tuple(np.array(settings.mesh_numbers).ravel())
-                nums = (("%d " * len(mesh_numbers)).strip()) % mesh_numbers
-                print(f"Mesh sampling: [ {nums} ]")
+            mesh_length = float(settings.mesh_numbers)
+            print(f"Length for sampling mesh generation: {mesh_length:.2f}")
         except TypeError:
-            print("Length for sampling mesh generation: %.2f" % settings.mesh_numbers)
+            mesh_numbers = tuple(np.ravel(settings.mesh_numbers))
+            nums = (("%d " * len(mesh_numbers)).strip()) % mesh_numbers
+            print(f"Mesh sampling: [ {nums} ]")
 
     sys.stdout.flush()
 
