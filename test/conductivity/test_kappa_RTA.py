748b016b297ef2be5a7cc78c317a336dc5767ffe
Atsushi Togo <atz.togo@gmail.com>
2023-12-25 16:00:38 +0900

    Update test_imag_self_energy.py

--- test/conductivity/test_kappa_RTA.py
+++ test/conductivity/test_kappa_RTA.py
@@ -264,9 +264,7 @@ def test_kappa_RTA_si_N_U(si_pbesol):
 
     # print(np.sum(gN), np.sum(gU))
     np.testing.assert_allclose(
-        np.sum([np.ravel(gN_ref), np.ravel(gU_ref)], axis=0),
-        gN.ravel() + gU.ravel(),
-        atol=1e-2,
+        np.ravel(gN_ref) + np.ravel(gU_ref), gN.ravel() + gU.ravel(), atol=1e-2
     )
     np.testing.assert_allclose(np.ravel(gN_ref), gN.ravel(), atol=1e-3)
     np.testing.assert_allclose(np.ravel(gU_ref), gU.ravel(), atol=1e-3)
