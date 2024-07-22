a77b73a5d2ff05978f44bbce0c10ab9bfd75bf12
Atsushi Togo <atz.togo@gmail.com>
2024-07-22 13:52:44 +0900

    Set version 3.3.2

--- doc/changelog.md
+++ doc/changelog.md
@@ -2,6 +2,11 @@
 
 # Change Log
 
+## Jul-22-2024: Version 3.3.2
+
+- Minor fix of `phono3py.load` function for reading displacements from
+  `phono3py_disp.yaml` like file that doesn't contain forces.
+
 ## Jul-8-2024: Version 3.3.1
 
 - Major refactoring to isolate reciprocal space grid code.
--- test/cui/test_phono3py_load.py
+++ test/cui/test_phono3py_load.py
@@ -8,8 +8,8 @@ from phono3py import Phono3py
 def test_phono3py_load(si_pbesol_without_forcesets: Phono3py):
     """Test phono3py.load.
 
-    Check phono3py.load can read displacements from phono3py_disp.yaml like file that
-    doesn't contain forces.
+    Check phono3py.load can read displacements from phono3py_disp.yaml like file
+    that doesn't contain forces.
 
     """
     ph3 = si_pbesol_without_forcesets
