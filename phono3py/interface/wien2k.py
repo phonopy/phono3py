a2de5322c11eb155384073aced89fd556a44fcf9
Atsushi Togo <atz.togo@gmail.com>
2024-12-28 07:26:05 +0900

    Merge PR-323

---
+++ phono3py/interface/wien2k.py
@@ -0,0 +1,61 @@
+"""ALM interface for force constants calculation."""
+
+# Copyright (C) 2016 Atsushi Togo
+# All rights reserved.
+#
+# This file is part of phono3py.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in
+#   the documentation and/or other materials provided with the
+#   distribution.
+#
+# * Neither the name of the phonopy project nor the names of its
+#   contributors may be used to endorse or promote products derived
+#   from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from phono3py.phonon3.dataset import get_displacements_fc3
+
+
+def get_fc3_calc_dataset_wien2k(
+    force_filenames,
+    supercell,
+    disp_dataset,
+    wien2k_P1_mode=False,
+    symmetry_tolerance=None,
+    verbose=False,
+):
+    """Read Wien2k output files and parse force sets."""
+    from phonopy.interface.wien2k import parse_set_of_forces
+
+    #    disps, _ = get_displacements_and_forces_fc3(disp_dataset)
+    disps = get_displacements_fc3(disp_dataset)
+    force_sets = parse_set_of_forces(
+        disps,
+        force_filenames,
+        supercell,
+        wien2k_P1_mode=wien2k_P1_mode,
+        symmetry_tolerance=symmetry_tolerance,
+        verbose=verbose,
+    )
+    return {"forces": force_sets}
